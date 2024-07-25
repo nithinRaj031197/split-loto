3d15632cb86efb9417b79816f93289e776f6c0f2
Keshav Verma <keshavverma532@gmail.com>
2024-07-25 15:52:01 +0530

    Added basic UI for landing screen

--- app/components/Topbar.tsx
+++ app/components/Topbar.tsx
@@ -1,20 +1,13 @@
 const TopBar = () => {
   return (
-    <div className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <div className="text-2xl font-bold">MyLogo</div>
+    <div className=" text-black p-4 flex justify-between items-center">
+      <div className="flex gap-4">
+          <div className="text-2xl font-bold">💵</div>
+          <div className="text-2xl font-bold">ItsSplit</div>
+      </div>
       <nav className="flex gap-4">
-        <a href="#home" className="hover:underline">
-          Home
-        </a>
-        <a href="#about" className="hover:underline">
-          About
-        </a>
-        <a href="#services" className="hover:underline">
-          Services
-        </a>
-        <a href="#contact" className="hover:underline">
-          Contact
-        </a>
+        <button>Log in</button>
+        <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-200">Sign up</button>
       </nav>
     </div>
   );
